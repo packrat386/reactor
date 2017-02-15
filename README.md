627f683376ead3754f6b5330a09f701e986053c9
Aidan Coyle <packrat386@gmail.com>
2017-02-14 21:42:37 -0600

    Stage 3
    We have proper option parsing for options that change every time, and a
    config for relatively static data. This may be overkill for such a tiny
    thing, but it works pretty nicely, which is nice.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Reactor
 
 Posts thumbs-ups on issues. Example repo for a tech talk.
+
+## Usage
+```
+Usage: reactor ISSUE_URL [options]
+    -c, --config PATH                Path to config, defaults to ~/.reactor.yml
+    -m, --message MESSAGE            Path to message, defaults to ":+1:"
+    -h, --help                       Show this message
+```
--- lib/reactor/client.rb
+++ lib/reactor/client.rb
@@ -1,14 +1,16 @@
 require 'octokit'
+require 'yaml'
 
 module Reactor
   class Client
-    def initialize(token:)
+    def initialize(config:)
+      token = YAML.safe_load(File.read(config))['token']
       @octokit = Octokit::Client.new(access_token: token)
     end
 
-    def react(issue_url:, comment: ':+1:')
+    def react(issue_url:, message:)
       repo = Octokit::Repository.from_url(issue_url)
-      @octokit.add_comment(repo, issue_url.split('/').last, comment)
+      @octokit.add_comment(repo, issue_url.split('/').last, message)
     end
   end
 end
